5ca9513565594a5fc2613f11b2d08ffd0521457e
Nicholas <nicholas.allio@me.com>
2019-02-20 23:23:10 +0100

    Tuned eslint configuration

--- .eslintrc.js
+++ .eslintrc.js
@@ -6,7 +6,9 @@ module.exports = {
   extends: "airbnb",
   globals: {
     Atomics: "readonly",
-    SharedArrayBuffer: "readonly"
+    SharedArrayBuffer: "readonly",
+    fetch: true,
+    __DEV__: true
   },
   parserOptions: {
     ecmaFeatures: {
@@ -15,6 +17,24 @@ module.exports = {
     ecmaVersion: 2018,
     sourceType: "module"
   },
-  plugins: ["react"],
-  rules: {}
+  plugins: ["react", "react-native"],
+  rules: {
+    "react/prefer-stateless-function": 1,
+    "react/jsx-filename-extension": 0,
+    "react-native/no-unused-styles": 2,
+    "react-native/split-platform-components": 2,
+    "react-native/no-inline-styles": 0,
+    "react-native/no-color-literals": 0,
+    "react/prop-types": [2, { ignore: ["navigator"] }],
+    quotes: [2, "single", { avoidEscape: true, allowTemplateLiterals: true }],
+    "class-methods-use-this": 0,
+    "react/no-unused-prop-types": [1, { skipShapeProps: true }]
+  },
+  settings: {
+    "import/resolver": {
+      node: {
+        extensions: [".js", ".android.js", ".ios.js"]
+      }
+    }
+  }
 };
