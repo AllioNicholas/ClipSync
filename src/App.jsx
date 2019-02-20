48bea431a13b377c2404daed0a522e0e6a769651
Nicholas <nicholas.allio@me.com>
2019-02-20 23:23:36 +0100

    Refactor component to be pure component

--- src/App.jsx
+++ src/App.jsx
@@ -3,18 +3,16 @@ import styled from 'styled-components';
 import { Text, View } from 'react-native';
 
 const StyledView = styled(View)`
-  flex: 1,
-  backgroundColor: '#fff',
-  alignItems: 'center',
-  justifyContent: 'center',
+  flex: 1;
+  backgroundcolor: #fff;
+  alignitems: center;
+  justifycontent: center;
 `;
 
-export default class App extends React.Component {
-  render() {
-    return (
-      <StyledView>
-        <Text>Open up App.js to start working on your app!</Text>
-      </StyledView>
-    );
-  }
-}
+const App = () => (
+  <StyledView>
+    <Text>Open up App.js to start working on your app!</Text>
+  </StyledView>
+);
+
+export default App;
