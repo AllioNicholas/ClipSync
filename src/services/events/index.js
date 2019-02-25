a8fefdf01cdaf53169b2a2683a0eeb79e8151ef2
Nicholas <nicholas.allio@me.com>
2019-02-25 23:54:24 +0100

    Implemented events to manage event tracking. To be tested

--- src/services/events/index.js
+++ src/services/events/index.js
@@ -1,1 +1,28 @@
 // @flow strict
+import { AsyncStorage } from 'react-native';
+
+const EVENTS_STORAGE_KEY = 'com.allio.nicholas.events';
+
+type Event = {
+  timestamp: string,
+  name: string,
+};
+
+type Log = {
+  [string]: Event,
+};
+
+export const trackEvent = (event: Event) => {
+  return AsyncStorage.mergeItem(
+    EVENTS_STORAGE_KEY,
+    JSON.stringify({
+      [event.timestamp]: event.name,
+    }),
+  );
+};
+
+export const getLog = () => AsyncStorage.getItem(EVENTS_STORAGE_KEY).then(JSON.parse);
+
+export const clearEvents = () => {
+  AsyncStorage.clear();
+};
