abba545e9cd2c72ba0d17ea8073f4d8371249620
Nicholas <nicholas.allio@me.com>
2019-02-28 00:25:06 +0100

    Added fake events for demonstration purposes

--- src/services/events/index.js
+++ src/services/events/index.js
@@ -12,6 +12,44 @@ type Log = {
   [string]: Event,
 };
 
+export const POSITIVE_EVENTS = {
+  GOOD_FOUL: {
+    timestamp: '1551309169',
+    name: 'good foul',
+  },
+  GOOD_NO_CALL: {
+    timestamp: '1551309170',
+    name: 'good no call',
+  },
+  GOOD_TRAVELING: {
+    timestamp: '1551309171',
+    name: 'good travelling',
+  },
+  GOOD_MECHANICS: {
+    timestamp: '1551309172',
+    name: 'good mechanics',
+  },
+};
+
+export const NEGATIVE_EVENTS = {
+  WRONG_MECHANICS: {
+    timestamp: '1551309173',
+    name: 'wrong mechanics',
+  },
+  WARNING_MISSED: {
+    timestamp: '1551309174',
+    name: 'warning missed',
+  },
+  WRONG_TRAVELLING: {
+    timestamp: '1551309175',
+    name: 'wrong travelling',
+  },
+  WRONG_RESET: {
+    timestamp: '1551309176',
+    name: 'wrong reset 14',
+  },
+};
+
 export const trackEvent = (event: Event) => {
   return AsyncStorage.mergeItem(
     EVENTS_STORAGE_KEY,
@@ -21,7 +59,7 @@ export const trackEvent = (event: Event) => {
   );
 };
 
-export const getLog = () => AsyncStorage.getItem(EVENTS_STORAGE_KEY).then(JSON.parse);
+export const getLog = (): Log => AsyncStorage.getItem(EVENTS_STORAGE_KEY).then(JSON.parse);
 
 export const clearEvents = () => {
   AsyncStorage.clear();
