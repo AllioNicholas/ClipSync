f3461e7b0033f635012b3393a01fef30259159d1
Nicholas <nicholas.allio@me.com>
2019-03-01 00:20:29 +0100

    Added provier time to app

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,18 @@
 import React from 'react';
 import styled from 'styled-components/native';
 import Content from './components/Content';
+import * as TimeContext from './services/time/context';
 
 const StyledContainer = styled.View`
   flex: 1;
 `;
 
 const App = () => (
-  <StyledContainer>
-    <Content />
-  </StyledContainer>
+  <TimeContext.Provider>
+    <StyledContainer>
+      <Content />
+    </StyledContainer>
+  </TimeContext.Provider>
 );
 
 export default App;
