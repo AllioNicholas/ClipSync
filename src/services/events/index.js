d21c70b9e18442ed1c54386e6ec4f99333fe4b05
Nicholas <nicholas.allio@me.com>
2019-03-01 01:37:00 +0100

    Adjusted logic to track event: now possible to track multiple events in same second

--- src/services/events/index.js
+++ src/services/events/index.js
@@ -14,52 +14,59 @@ type Log = {
 
 export const POSITIVE_EVENTS = {
   GOOD_FOUL: {
-    timestamp: '1551309169',
     name: 'good foul',
   },
   GOOD_NO_CALL: {
-    timestamp: '1551309170',
     name: 'good no call',
   },
   GOOD_TRAVELING: {
-    timestamp: '1551309171',
     name: 'good travelling',
   },
   GOOD_MECHANICS: {
-    timestamp: '1551309172',
     name: 'good mechanics',
   },
 };
 
 export const NEGATIVE_EVENTS = {
   WRONG_MECHANICS: {
-    timestamp: '1551309173',
     name: 'wrong mechanics',
   },
   WARNING_MISSED: {
-    timestamp: '1551309174',
     name: 'warning missed',
   },
   WRONG_TRAVELLING: {
-    timestamp: '1551309175',
     name: 'wrong travelling',
   },
   WRONG_RESET: {
-    timestamp: '1551309176',
     name: 'wrong reset 14',
   },
 };
 
-export const trackEvent = (event: Event) => {
-  return AsyncStorage.mergeItem(
-    EVENTS_STORAGE_KEY,
-    JSON.stringify({
-      [event.timestamp]: event.name,
-    }),
-  );
-};
-
 export const getLog = (): Log => AsyncStorage.getItem(EVENTS_STORAGE_KEY).then(JSON.parse);
+
+export const trackEvent = (event: Event) => {
+  getLog().then(events => {
+    if (events) {
+      const existingEvent = events[event.timestamp];
+      if (existingEvent) {
+        const eventsArray = [existingEvent, event.name];
+        return AsyncStorage.mergeItem(
+          EVENTS_STORAGE_KEY,
+          JSON.stringify({
+            [event.timestamp]: eventsArray,
+          }),
+        );
+      }
+    }
+
+    return AsyncStorage.mergeItem(
+      EVENTS_STORAGE_KEY,
+      JSON.stringify({
+        [event.timestamp]: event.name,
+      }),
+    );
+  });
+};
 
 export const clearEvents = () => {
   AsyncStorage.clear();
